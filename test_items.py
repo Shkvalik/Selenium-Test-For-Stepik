7d95f0479bd6cd016c01efaf305d92469afa006f
Shkvalik <wal2004yandex.ru@gmail.com>
2021-11-14 12:58:33 +0200

    Add all files

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import time
+
+
+def test_change_user_language(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(5)  # Сократил время на проверку, чтобы не занимать время
+    active_button = browser.find_element_by_css_selector("button[class='btn btn-lg btn-primary btn-add-to-basket']")
+    assert active_button, print('Button is not found')
+
+# Pytest command
+# pytest --language=fr test_items.py
+# pytest --language=eu test_items.py
+# pytest --language=ru test_items.py
